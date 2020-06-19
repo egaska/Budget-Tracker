c5ef7e9755052a87488ebad9ebfad38121ede117
Emily Gaska <emilygaska@gmail.com>
2020-06-18 20:39:42 -0500

    Added the basic set up for service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,13 +5,14 @@ const FILES_TO_CACHE = [
   "/db.js",
   "/style.css",
 ];
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
-      .open(STATIC_CACHE)
+      .open(CACHE_NAME)
       .then((cache) => {
         cache.addAll(FILES_TO_CACHE);
         console.log("Your files were pre-cached successfully!");
@@ -22,7 +23,7 @@ self.addEventListener("install", (event) => {
 
 // activate
 self.addEventListener("activate", function (event) {
-    evt.waitUntil(
+    event.waitUntil(
       caches.keys().then((keyList) => {
         return Promise.all(
           keyList.map((key) => {
@@ -36,5 +37,41 @@ self.addEventListener("activate", function (event) {
     );
   
     self.clients.claim();
+  });
+  
+  // fetch
+self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then((cache) => {
+            return fetch(evt.request)
+              .then((response) => {
+                // If the response was good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+  
+                return response;
+              })
+              .catch((err) => {
+                // Network request failed, try to get it from the cache.
+                return cache.match(evt.request);
+              });
+          })
+          .catch((err) => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(evt.request).then((response) => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
   });
   
